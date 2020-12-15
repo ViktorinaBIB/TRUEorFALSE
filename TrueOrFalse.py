ab2a406eee73b699aaf0c465ecc7b31ffb124c9e
Lisa-olesya <75995094+Lisa-olesya@users.noreply.github.com>
2020-12-15 22:41:34 +0300

    Create TrueOrFalse.py

---
+++ TrueOrFalse.py
@@ -0,0 +1,10 @@
+# TRUEorFALSE
+from tkinter import *
+from PIL import ImageTk,Image
+root = Tk()
+root.title('Правда или Ложь')
+image = ImageTk.PhotoImage(file = "C:\\Users\\olesya\\Pictures\\Screenshots\\RkNC2XOxYdM.png")
+root.geometry('400x400')
+background_label = Label(root, image=image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1) 
+count=0
