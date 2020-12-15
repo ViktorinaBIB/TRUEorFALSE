bcb259f3a0caf431f04e467bd7ef8889992070c8
Lisa-olesya <75995094+Lisa-olesya@users.noreply.github.com>
2020-12-15 23:40:43 +0300

    Update TrueOrFalse.py

--- TrueOrFalse.py
+++ TrueOrFalse.py
@@ -17,8 +17,8 @@ def clear():
 
 def que1():
     question = Label(root, text = "Детёныши жирафов рождаются без пятен")
-    btn_tr = Button(root, text = "правда", bg="OliveDrab2", fg="black", command=lambda: next_w(que2))
-    btn_fa = Button(root, text = "ложь", bg="red3", fg="black", command=lambda: next_r(que2))
+    btn_tr = Button(root, text = "Правда", bg="OliveDrab2", fg="black", command=lambda: next_w(que2))
+    btn_fa = Button(root, text = "Ложь", bg="red3", fg="black", command=lambda: next_r(que2))
     question.pack()
     btn_tr.pack()
     btn_fa.pack()
@@ -35,8 +35,8 @@ def que1():
 
 def que2():
     question = Label(root, text = "Вы не чувствуете запахов, пока спите")
-    btn_tr = Button(root, text = "правда", bg="OliveDrab2", fg="black", command=lambda: next_r(que2))
-    btn_fa = Button(root, text = "ложь", bg="red3", fg="black", command=lambda: next_w(que2))
+    btn_tr = Button(root, text = "Правда", bg="OliveDrab2", fg="black", command=lambda: next_r(que3))
+    btn_fa = Button(root, text = "Ложь", bg="red3", fg="black", command=lambda: next_w(que3))
     question.grid(row=3)
     btn_tr.grid(row=5)
     btn_fa.grid(row=6)
@@ -45,11 +45,40 @@ def que2():
         global count
         count+=1
         clear()
-        que2()
+        que3()
     
     def next_w(que3):
         clear()
-        que2()
+        que3()
 
 def que3():
-    pass
+    question = Label(root, text= "Утиное крякание не дает эхо")
+    btn1 = Button (root, text="Правда", bg="OliveDrab2", fg="black", command=lambda: next_r(que4))
+    btn2 = Button (root, text="Ложь", bg="OliveDrab2", fg="black", command=lambda: next_w(que4))
+    question.pack()
+    btn1.pack()
+    btn2.pack()
+    def next_r(que4):
+        global count 
+        count += 1 
+        clear()
+        que4()
+    def next_w(que4):
+        clear()
+        que4()
+
+def que4():
+    question = Label(root, text= "Самое ядовитое существо на планете змея")
+    btn1 = Button (root, text="Правда",font = "Calibri", bg="OliveDrab2", fg="black", command=lambda: next_w(que2))
+    btn2 = Button (root, text="Ложь", font = "Calibri", bg="OliveDrab2", fg="black", command=lambda: next_r(que2))
+    question.pack()
+    btn1.pack()
+    btn2.pack()
+    def next_r(que5):
+        global count 
+        count += 1 
+        clear()
+        que5()
+    def next_w(que5):
+        clear()
+        que5()
