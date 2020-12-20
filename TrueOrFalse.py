30348c142c0e3da76e230c9cbb46c51a8f950b2e
Lisa-olesya <75995094+Lisa-olesya@users.noreply.github.com>
2020-12-21 00:54:25 +0300

    Update TrueOrFalse.py

--- TrueOrFalse.py
+++ TrueOrFalse.py
@@ -4,7 +4,7 @@ from PIL import ImageTk,Image
 root = Tk()
 root.title('Правда или Ложь')
 image = ImageTk.PhotoImage(file = "C:\\Users\\olesya\\Pictures\\Screenshots\\RkNC2XOxYdM.png")
-root.geometry('1000x800')
+root.geometry('1200x800')
 background_label = Label(root, image=image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1) 
 count=0
@@ -21,9 +21,6 @@ def que1():
     question = Label(root, text = "Детёныши жирафов рождаются без пятен", font = ("KacstArt", 30))
     btn_tr = Button(root, text = "Правда", font = ("KacstArt", 30), bg="OliveDrab2", fg="black", command=lambda: next_w(que2))
     btn_fa = Button(root, text = "Ложь", font = ("KacstArt", 30), bg="red3", fg="black", command=lambda: next_r(que2))
-    question = Label(root, text = "Детёныши жирафов рождаются без пятен", font = ("KacstArt", 40))
-    btn_tr = Button(root, text = "Правда", font = ("KacstArt", 40), bg="OliveDrab2", fg="black", command=lambda: next_w(que2))
-    btn_fa = Button(root, text = "Ложь", font = ("KacstArt", 40), bg="red3", fg="black", command=lambda: next_r(que2))
     question.pack()
     btn_tr.pack()
     btn_fa.pack()
@@ -63,8 +60,8 @@ def que2():
 
 def que3():
     '''Отображает 3 вопрос и кнопки для ответа'''
-    question = Label(root, text= "Утиное крякание не дает эхо",font = ("KacstArt",40))
-    btn1 = Button (root, text="Правда", font = ("KacstArt",40), bg="OliveDrab2", fg="black", command=lambda: next_r(que4))
+    question = Label(root, text= "Утиное крякание не дает эхо",font = ("KacstArt",30))
+    btn1 = Button (root, text="Правда", font = ("KacstArt",30), bg="OliveDrab2", fg="black", command=lambda: next_r(que4))
     btn2 = Button (root, text="Ложь", font = ("KacstArt",30), bg="red3", fg="black", command=lambda: next_w(que4))
     question.pack()
     btn1.pack()
@@ -82,9 +79,9 @@ def que3():
 
 def que4():
     '''Отображает 4 вопрос и кнопки для ответа'''
-    question = Label(root, text= "Самое ядовитое существо на планете является змеёй",font = ("KacstArt",35))
-    btn1 = Button (root, text="Правда",font = ("KacstArt",35), bg="OliveDrab2", fg="black", command=lambda: next_w(que5))
-    btn2 = Button (root, text="Ложь", font = ("KacstArt",40), bg="red3", fg="black", command=lambda: next_r(que5))
+    question = Label(root, text= "Самое ядовитое существо на планете является змеёй",font = ("KacstArt",30))
+    btn1 = Button (root, text="Правда",font = ("KacstArt",30), bg="OliveDrab2", fg="black", command=lambda: next_w(que5))
+    btn2 = Button (root, text="Ложь", font = ("KacstArt",30), bg="red3", fg="black", command=lambda: next_r(que5))
     question.pack()
     btn1.pack()
     btn2.pack()
@@ -121,7 +118,7 @@ def que5():
 def que6():
     '''Отображает шестой вопрос и кнопки для ответа'''
     question = Label(root, text= "Несмоторя на внушительные размеры белого кита,", font = ("KacstArt", 30))
-    question2 = Label(root, text = "его сердце всего лишь в 2 раза больше человеческого", font = ("KacstArt", 40))
+    question2 = Label(root, text = "его сердце всего лишь в 2 раза больше человеческого", font = ("KacstArt", 30))
     btn1 = Button (root, text="Правда", font = ("KacstArt", 30), bg="OliveDrab2", fg="black", command=lambda: next_w(que7))
     btn2 = Button (root, text="Ложь", font = ("KacstArt", 30), bg="red3", fg="black", command=lambda: next_r(que7))
     question.pack()
@@ -141,7 +138,7 @@ def que6():
 
 def que7():
     '''Отображает седьмой вопрос и кнопки для ответа'''
-    question = Label(root, text= "Галапагосские черепахи - единственные существа в мире,", font = ("KacstArt", 40))
+    question = Label(root, text= "Галапагосские черепахи - единственные существа в мире,", font = ("KacstArt", 30))
     question2 = Label(root, text = "у которых самцы выращивают потомство", font = ("KacstArt", 30))
     btn1 = Button (root, text="Правда", font = ("KacstArt", 30), bg="OliveDrab2", fg="black", command=lambda: next_w(que8))
     btn2 = Button (root, text="Ложь", font = ("KacstArt", 30), bg="red3", fg="black", command=lambda: next_r(que8))
@@ -162,9 +159,9 @@ def que7():
 
 def que8():
     '''Отображает 8 вопрос и кнопки для ответа'''
-    question = Label(root, text="Самая короткая война в мире длилась около двух дней",font = ("KacstArt",40))
-    btn1=Button(root,text="Правда",font = ("KacstArt",40), bg="OliveDrab2",fg="black",command=lambda:next_w(que9))
-    btn2=Button(root,text="Ложь",font = ("KacstArt",40), bg="red3",fg="black",command=lambda:next_r(que9))
+    question = Label(root, text="Самая короткая война в мире длилась около двух дней",font = ("KacstArt",30))
+    btn1=Button(root,text="Правда",font = ("KacstArt",30), bg="OliveDrab2",fg="black",command=lambda:next_w(que9))
+    btn2=Button(root,text="Ложь",font = ("KacstArt",30), bg="red3",fg="black",command=lambda:next_r(que9))
     question.pack()
     btn1.pack()
     btn2.pack()
@@ -181,9 +178,9 @@ def que8():
 
 def que9():
     '''Отображает 9 вопрос и кнопки для ответа'''
-    question = Label(root, text="Существуют рыбы, которые могут жить на суше",font = ("KacstArt",40))
-    btn1=Button(root,text="Правда",font = ("KacstArt",40), bg="OliveDrab2",fg="black",command=lambda:next_r(que10))
-    btn2=Button(root,text="Ложь",font = ("KacstArt",40), bg="red3",fg="black",command=lambda:next_w(que10))
+    question = Label(root, text="Существуют рыбы, которые могут жить на суше",font = ("KacstArt",30))
+    btn1=Button(root,text="Правда",font = ("KacstArt",30), bg="OliveDrab2",fg="black",command=lambda:next_r(que10))
+    btn2=Button(root,text="Ложь",font = ("KacstArt",30), bg="red3",fg="black",command=lambda:next_w(que10))
     question.pack()
     btn1.pack()
     btn2.pack()
@@ -200,9 +197,9 @@ def que9():
 
 def que10():
     '''Отображает 10 вопрос и кнопки для ответа'''
-    question = Label(root, text="Больше всего озер расположено в Канаде",font = ("KacstArt",40))
-    btn1=Button(root,text="Правда",font = ("KacstArt",40), bg="OliveDrab2",fg="black",command=lambda:next_r(res))
-    btn2=Button(root,text="Ложь",font = ("KacstArt",40), bg="red3",fg="black",command=lambda:next_w(res))
+    question = Label(root, text="Больше всего озер расположено в Канаде",font = ("KacstArt",30))
+    btn1=Button(root,text="Правда",font = ("KacstArt",30), bg="OliveDrab2",fg="black",command=lambda:next_r(res))
+    btn2=Button(root,text="Ложь",font = ("KacstArt",30), bg="red3",fg="black",command=lambda:next_w(res))
     question.pack()
     btn1.pack()
     btn2.pack()
@@ -219,7 +216,7 @@ def que10():
 
 def res():
     ''' Отображает результат'''
-    result = Label(root, text='Результат: '+ str(count) + ' из 10')
+    result = Label(root, text='Результат: '+ str(count) + ' из 10',font = ("KacstArt",30))
     if (count<=10) and (count>=8):
         description = "Поздравляем! Прекрасный результат!"
     elif (count<=7) and (count>=5):
@@ -227,8 +224,16 @@ def res():
     elif (count<=4) and (count>=0):
         description = "Не расстраивайтесь! Вы можете попробовать ещё раз."
     result.pack()
-    description=Label(root, text=str(description),font = ("KacstArt",40))
+    description=Label(root, text=str(description),font = ("KacstArt",30))
     description.pack()
+    btn=Button(root,text="Начать заново",font = ("KacstArt",30),fg="black",command=lambda:repeat())
+    btn.pack()
+
+def repeat():
+    '''Обнуляет счет, очищает экран и начинает тест заново'''
+    global count
+    count=0
+    clear()
+    que1()
 que1()
-btn=Button(root,text="Начать заново",font="Calibri",fg="black",command=lambda:que1())
 root.mainloop()
